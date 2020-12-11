785074a4001acc99f058e8e76964563b50f23e29
GtrCraft <nickvanbokhorst@hotmail.com>
2020-12-11 09:21:41 +0100

    surya: Set lmkd props
    Change-Id: I3277de324aa8399553e0c65908a3edad507b0db0

--- product_prop.mk
+++ product_prop.mk
@@ -27,6 +27,20 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1
+    
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.low_ram=false \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true \
+    ro.lmk.log_stats=true
 
 # Recorder
 PRODUCT_PRODUCT_PROPERTIES += \
