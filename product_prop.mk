3b345dcb1ba33c68d0049160ea7f67d978790e2f
GtrCraft <nickvanbokhorst@hotmail.com>
2020-12-21 13:27:58 +0100

    Revert "surya: Set lmkd props"
    This reverts commit 785074a4001acc99f058e8e76964563b50f23e29.
    
    Change-Id: I5ac0a96357aa18dd408dfb8b5b68bf3da53d6312

--- product_prop.mk
+++ product_prop.mk
@@ -44,20 +44,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
     
-# LMKD
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.low_ram=false \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true \
-    ro.lmk.log_stats=true
-
 # Recorder
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.audio.sdk.fluencetype=fluence \
