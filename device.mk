48474b27dff1200550c1b64b29f75e69e398b9c8
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-04 11:07:21 +0300

    surya: parts: Get rid of cutout settings
    * it doesn't work that great and it should be provided by the system, if desired
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ie1bd62835a8c594e993e1b410ac17859d551830f

--- device.mk
+++ device.mk
@@ -129,15 +129,11 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag
 
-# Notch hide overlay
-PRODUCT_PACKAGES += \
-    NotchHideOverlay \
-    NotchHideOverlaySystemUI
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+    
 
 # Overlays - override vendor ones
 PRODUCT_PACKAGES += \
@@ -145,7 +141,7 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     DevicesOverlay \
     DevicesAndroidOverlay
-
+    
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml \
