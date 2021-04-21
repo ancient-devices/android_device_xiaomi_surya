03a75d0f8222eccaa0ec60eba817a7ff6e5aabac
GtrCraft <nickvanbokhorst@hotmail.com>
2021-04-21 10:30:46 +0200

    surya: Use optimus kernel defconfig
    * Also use clang 12.0.5
    
    Change-Id: I2931576cd5be790de705fd7526fc5bb6088e9741

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,9 +39,9 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya
 TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/surya-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
-TARGET_KERNEL_USE_LATEST_CLANG := true
+TARGET_KERNEL_CLANG_VERSION := 7284624
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
 # Partitions
