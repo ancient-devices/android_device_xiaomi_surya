15a3fd9bdad9ee6aa7be2ad22b4e32f7801199b6
Kubilhadinanta <hadinatasurya@gmail.com>
2021-04-22 18:38:43 +0700

    surya: bringup AncientOS

--- ancient_surya.mk
+++ ancient_surya.mk
@@ -17,19 +17,30 @@
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit from common WaveOS configuration
+# Inherit from common AncientOS configuration
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/wave/configs/common.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+
+# Pixel Charger
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+
+# Use Background Blur
+TARGET_USES_BLUR := true
 
 # Device identifier
-PRODUCT_NAME := wave_surya
+PRODUCT_NAME := ancient_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X3
+PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
